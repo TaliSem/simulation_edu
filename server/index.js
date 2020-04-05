6ea8177c63e1930f241e295ae67832c5f1aaa63c
TaliSem <43914178+TaliSem@users.noreply.github.com>
2020-04-05 19:31:51 +0300

    indx

--- server/index.js
+++ server/index.js
@@ -10,6 +10,71 @@ app.get("/api", (req, res) => {
   res.send({res:"result from server"});
 });
 
+//Screen 2
+app.post("/users/register", (req, res) => {
+  console.log(" users register is accessed");
+  res.sendStatus(201);
+});
+
+//Screen 3
+app.get("/simulations/join", (req, res) => {
+  console.log("simulation login is accessed");
+  res.sendStatus(200);
+});
+
+//Screen 4
+app.get("/simulation", (req, res) => {
+  console.log("simulation list is accessed");
+  res.send({
+  simulationName:"simulationName",
+  simulationDescription:"simulationDescription1",
+  simulationTime:"simulationTime10"
+
+});
+});
+
+app.post("/simulationInstance", (req, res) => {
+  console.log("simulationInstance - Created UUID");
+  res.sendStatus(201);
+});
+
+//Screen 5
+app.get("/simulation/:id", (req, res) => {
+  console.log("simulation- get simulation simulation by id");
+  res.send({
+    simulationName:"simulationName1",
+    simulationDescription:"simulationDescription1",
+    simulationTime:"simulationTime1"
+  
+  });
+});
+
+app.put("/simulationUpdateRole", (req, res) => {
+  console.log("simulation Update Role");
+  res.send({
+    Role1:"Role1",
+    Role2:"Role2",
+  });
+});
+
+app.put("/simulationStart", (req, res) => {
+  console.log("simulationStart is accessed");
+  res.sendStatus(200);
+});
+
+//Screen 7
+app.put("/simulationNextLevel", (req, res) => {
+  console.log("simulation Next Level is accessed");
+  res.sendStatus(200);
+});
+
+app.put("/simulationStop", (req, res) => {
+  console.log("simulation Stop is accessed");
+  res.sendStatus(200);
+});
+
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
