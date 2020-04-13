ffb990caa3a9064f8b8485de836998365289bbd8
Tali Sem <taliyaacov@hotmail.com>
2020-04-13 22:43:24 +0300

    tali

--- server/index.js
+++ server/index.js
@@ -5,73 +5,91 @@ const app = express();
 // used for json inside body 
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-  console.log("root is accessed");
-  res.send({res:"result from server"});
-});
+ app.get("/api", (req, res) => {
+   console.log("root is accessed");
+   res.send({res:"result from server"});
+ });
 
-//Screen 2
-app.post("/users/register", (req, res) => {
-  console.log(" users register is accessed");
-  res.sendStatus(201);
-});
+//Slide 3- register to the simulations
 
-//Screen 3
-app.get("/simulations/join", (req, res) => {
-  console.log("simulation login is accessed");
-  res.sendStatus(200);
-});
+ app.post("/api/users/register", (req, res) => {
+   console.log(" users register is accessed");
+   res.sendStatus(201);
+ });
 
-//Screen 4
-app.get("/simulation", (req, res) => {
-  console.log("simulation list is accessed");
-  res.send({
-  simulationName:"simulationName",
-  simulationDescription:"simulationDescription1",
-  simulationTime:"simulationTime10"
+//Slide 13 -- Allows the other players to join the game (with the UUID)
 
-});
-});
+ app.get("/api/simulations/join", (req, res) => {
+   console.log("simulation login is accessed");
+   res.sendStatus(200);
+ });
 
-app.post("/simulationInstance", (req, res) => {
-  console.log("simulationInstance - Created UUID");
-  res.sendStatus(201);
-});
+//Slide 10-- simulation library only for the manager
 
-//Screen 5
-app.get("/simulation/:id", (req, res) => {
-  console.log("simulation- get simulation simulation by id");
-  res.send({
-    simulationName:"simulationName1",
-    simulationDescription:"simulationDescription1",
-    simulationTime:"simulationTime1"
+ app.get("/api/simulation", (req, res) => {
+   console.log("simulation list is accessed");
+   res.send(
+     [{
+    simulationName:"simulationName",
+    simulationDescription:"simulationDescription",
+    simulationTime:"simulationTime"
+      },
+  {
+    simulationName:"1simulationName",
+    simulationDescription:"1simulationDescription",
+    simulationTime:"1simulationTime"
+  },]);
+  });
+
+//Slide 11-- When the manager selects the simulation
+
+  app.post("/api/simulationInstance", (req, res) => {
+    console.log("simulationInstance - Created UUID");
+    res.sendStatus(201);
+  });
+
+//Slide 11 Details of the selected simulation
+
+  app.get("/api/simulation/:id", (req, res) => {
+    console.log("simulation- get simulation simulation by id");
+    res.send({
+      simulationName:"simulationName1",
+      simulationDescription:"simulationDescription1",
+      simulationTime:"simulationTime1"
   
+    });
   });
-});
 
-app.put("/simulationUpdateRole", (req, res) => {
-  console.log("simulation Update Role");
-  res.send({
-    Role1:"Role1",
-    Role2:"Role2",
+//Slide 12-- UpdateRole for simulation
+
+  app.put("/api/simulation/:id/UpdateUser", (req, res) => {
+    console.log("simulation Update Role");
+    res.send({
+      user1:"user1",
+      user2:"user2",
+    });
   });
-});
 
-app.put("/simulationStart", (req, res) => {
-  console.log("simulationStart is accessed");
-  res.sendStatus(200);
-});
+//Slide 16-- Start simulation
 
-//Screen 7
-app.put("/simulationNextLevel", (req, res) => {
-  console.log("simulation Next Level is accessed");
-  res.sendStatus(200);
-});
+  app.put("/api/simulation/:id/Start", (req, res) => {
+    console.log("simulationStart is accessed");
+    res.sendStatus(200);
+  });
 
-app.put("/simulationStop", (req, res) => {
-  console.log("simulation Stop is accessed");
-  res.sendStatus(200);
-});
+//Slide 18 - NextLevel
+
+ app.put("/api/simulation/:id/NextLevel", (req, res) => {
+    console.log("simulation Next Level is accessed");
+    res.sendStatus(200);
+  });
+
+//Slide 18 18--Stop
+
+  app.put("/api/simulation/:id/Stop", (req, res) => {
+    console.log("simulation Stop is accessed");
+    res.sendStatus(200);
+  });
 
 
 
