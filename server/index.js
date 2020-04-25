f7ae0b4de851e7e613d298f7defc7088d768bc3c
Tali Sem <taliyaacov@hotmail.com>
2020-04-25 09:40:38 +0300

    server final

--- server/index.js
+++ server/index.js
@@ -17,19 +17,13 @@ app.use(express.json());
    res.sendStatus(201);
  });
 
-//Slide 13 -- Allows the other players to join the game (with the UUID)
-
- app.get("/api/simulations/join", (req, res) => {
-   console.log("simulation  login is accessed");
-   res.sendStatus(200);
- });
-
 //Slide 10-- simulation library only for the manager
 
  app.get("/api/simulation", (req, res) => {
    console.log("simulation list is accessed");
    res.send(
      [{
+    id:1,
     simulationName:"simulationName",
     simulationDescription:"simulationDescription",
     simulationTime:"simulationTime",
@@ -37,6 +31,7 @@ app.use(express.json());
 
       },
   {
+    id:2,
     simulationName:"1simulationName",
     simulationDescription:"1simulationDescription",
     simulationTime:"1simulationTime",
@@ -47,34 +42,53 @@ app.use(express.json());
 //Slide 11-- When the manager selects the simulation
 // --- All info app.get would return, so that the client can use it to render the next screen.
 
-  app.get("/api/simulation/:id", (req, res) => {
+  app.get("/api/simulation/:id/createNew", (req, res) => {
     console.log("simulationInstance - Created UUID");
-    res.sendStatus(201);
+    res.send({res:"UUID"});
   });
-
-
 
 //Slide 11 Details of the selected simulation
 
   app.get("/api/simulation/:id", (req, res) => {
     console.log("simulation- get simulation simulation by id");
     res.send({
-      simulationName:"simulationName1",
-      simulationDescription:"simulationDescription1",
-      simulationTime:"simulationTime1",
+      id:1,
+      simulationName:"simulationName",
+      simulationDescription:"simulationDescription",
+      simulationTime:"simulationTime",
       numberOfParticipants:"numberOfParticipants"
-  
     });
   });
 
 //Slide 12-- UpdateRole for simulation - role to user
 
-  app.put("/api/simulation/:id/UpdateUser", (req, res) => {
+  app.put("/api/simulation/:id/UUID/UpdateUser", function (req, res, next) {
+    const queryUser =  req.body;
+    console.log(queryUser.role);
+    if (!queryUser.role)
+     next('route')
+    else next()
+  }, function (req, res, next) {
+    res.send(`The role updated - successfully`)
+  })
+  
+ 
+  //--I added the role name in URL So we can change the sim description by role
+
+  app.get("/api/simulation/:id/UUID/UpdateUser/:roleName", (req, res) => {
     console.log("simulation Update Role");
     res.send({
       Role1:"user1",
       Role2:"user2",
+      Role3:"user3",
     });
+  });
+
+  //Slide 13 -- Allows the other players to join the game (with the UUID)
+
+  app.get("/api/simulations/:id/join", (req, res) => {
+   console.log("simulation login is accessed");
+    res.sendStatus(200);
   });
 
 //Slide 16-- Start simulation
